08a34a5d5fb53bd428481d6e3bca77c5977b03cd
Nik-qa <badanov.qa@gmail.com>
2023-11-22 21:19:14 +0100

    Задание с чек-боксами

--- lessons/chapter_3/ls3.3_6.py
+++ lessons/chapter_3/ls3.3_6.py
@@ -1,7 +1,7 @@
 # Ответ
 
 # номер 8
-def test_guest_can_open_new_course(browser):
+# def test_guest_can_open_new_course(browser):
 # номер 5
-class TestLessonCreate():
-    def test_create_lesson(self, browser):
+# class TestLessonCreate():
+#     def test_create_lesson(self, browser):
