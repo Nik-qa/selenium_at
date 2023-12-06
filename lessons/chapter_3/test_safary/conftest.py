291316e380f6d3e14cc63af16d00bca26eb0669f
Nik-qa <badanov.qa@gmail.com>
2023-12-06 15:37:27 +0100

    conftest

--- lessons/chapter_3/test_safary/conftest.py
+++ lessons/chapter_3/test_safary/conftest.py
@@ -3,22 +3,26 @@ from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser_name", action="store", default="chrome",
+    parser.addoption("--browser_name", action="store", default='chrome',
                      help="Choose browser: chrome or firefox or safari")
+# если тут default='chrome' вместо 'chrome'  поставить None без кавычек, то будет ошибка
 
-
-@pytest.fixture
+@pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    # browser = None
+    browser = None
     if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
         browser = webdriver.Chrome()
     elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
     elif browser_name == "safari":
+        print("\nstart safari browser for test..")
         browser = webdriver.Safari()
     else:
         raise pytest.UsageError("--browser_name should be chrome, firefox, or safari")
 
     yield browser
     browser.quit()
+    # для запуска с нужным бразуером pytest -s -v --browser_name=safari test_cmd.py
